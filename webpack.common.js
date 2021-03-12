efa75b3f81339c3de7216869b7fa23b7fd486449
uazhar-code <uazhar.git@gmail.com>
2021-03-12 02:14:54 -0500

    Testing webpack loader path for resume

--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
         use: ["html-loader"]
       },
       {
-        test: /\.(jpg|jpeg|png|gif|svg|pdf)$/,
+        test: /\.(pdf|jpg|jpeg|png|gif|svg)$/,
         use: [
           {
             loader: "file-loader",
